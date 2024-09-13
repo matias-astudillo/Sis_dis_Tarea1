c4199ee8f6a87ac9b98f0759df70f300a5bcc9ef
matias-astudillo <astudillomatias89@gmail.com>
2024-09-13 18:33:16 -0300

    P2_2P_HASH

--- api/app.py
+++ api/app.py
@@ -4,23 +4,33 @@ import grpc
 import grpc_client
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis_container', port=6379)
+#cache = redis.Redis(host='redis_container', port=6379)
+
+redis_clients = [
+    redis.Redis(host='redis_container1', port=6379),
+    redis.Redis(host='redis_container2', port=6380),
+]
+
+def get_redis_client(key: str):
+    index = hash(key) % len(redis_clients)
+    return redis_clients[index] , index
 
 @app.route('/resolve', methods=['POST'])
 def resolve_domain():
     domain = request.json['domain']
+    cache, partition = get_redis_client(domain)
     
     # Buscar en Redis
     ip = cache.get(domain)
     if ip:
-        return jsonify({"domain": domain, "ip": ip.decode('utf-8'), "source": "cache"})
+        return jsonify({"domain": domain, "ip": ip.decode('utf-8'), "source": "cache", "partition": partition})
 
     # No está en caché, hacemos resolución gRPC
     ip = grpc_client.resolve_domain_via_grpc(domain)
 
     # Almacenar en Redis
     cache.set(domain, ip)
-    return jsonify({"domain": domain, "ip": ip, "source": "dns"})
+    return jsonify({"domain": domain, "ip": ip, "source": "dns", "partition": partition})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
--- simulator/simulator.py
+++ simulator/simulator.py
@@ -24,25 +24,30 @@ api_url = "http://api_container:5000/resolve"
 response_times = []
 hits = 0
 misses = 0
+partition_stats = {0: 0, 1: 0}
 
 for _ in range(500):  
     domain = random.choice(domains)
     start_time = time.time()  # Inicia el temporizador
     response = requests.post(api_url, json={"domain": domain})
     end_time = time.time()  # Detiene el temporizador
+    response_data = response.json()
     
     # Mide el tiempo de respuesta
     response_time = end_time - start_time
     response_times.append(response_time)
+
+    # Actualizar los balances de carga
+    partition = response_data["partition"]
+    partition_stats[partition] += 1
     
     # Analiza la respuesta para determinar si es un hit o miss
-    response_data = response.json()
     if response_data['source'] == 'cache':
         hits += 1
     else:
         misses += 1
     
-    #print(f"Sent domain: {domain}, Response: {response.text}, Response Time: {response_time:.4f} seconds")
+    #print(f"Sent domain: {domain}, Response: {response.text}, Response Time: {response_time:.4f} seconds, Partition: {partition}")
 
 # Calcula el promedio y la desviación estándar de los tiempos de respuesta
 average_response_time = statistics.mean(response_times)
@@ -58,3 +63,4 @@ print(f"\nAverage Response Time: {average_response_time:.4f} seconds")
 print(f"Standard Deviation of Response Time: {std_deviation_response_time:.4f} seconds")
 print(f"Hit Rate: {hit_rate:.2f}")
 print(f"Miss Rate: {miss_rate:.2f}")
+print("Balances de carga:", partition_stats)
